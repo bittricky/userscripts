c392766e47c9ef195b69240d43f1c7a1b1eb5dc7
Mitul Patel <me@mitulpa.tel>
2025-05-19 14:47:06 -0400

    fix(userscript): remove comments

--- hide-x-follow-activity/hide-x-follow-activity.js
+++ hide-x-follow-activity/hide-x-follow-activity.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         X (Twitter) Hide Follower Activity
+// @name         X Hide Follower Activity
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Completely hides the followers and following counts on X (Twitter) profiles
+// @description  Completely hides the followers and following counts on X profiles
 // @author       Mitul Patel
 // @match        https://x.com/*
 // @match        https://twitter.com/*
@@ -20,11 +20,9 @@
   }
 
   const hideFollowersCss = `
-    /* Hide following and followers links on X profiles */
     a[href$="/following"],
     a[href$="/followers"],
     a[href*="/verified_followers"],
-    /* Hide the entire container if needed */
     div.r-13awgt0.r-18u37iz.r-1w6e6rj {
       display: none !important;
     }
